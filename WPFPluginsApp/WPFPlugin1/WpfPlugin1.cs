0a296b4c72db1de0e3b69050353cc19b5e2b2ca5
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-08-04 11:42:43 +0200

    Działający ModelLocator w Plugin1

--- WPFPluginsApp/WPFPlugin1/WpfPlugin1.cs
+++ WPFPluginsApp/WPFPlugin1/WpfPlugin1.cs
@@ -13,7 +13,7 @@ namespace WPFPlugin1
     {
         public WpfPlugin1(IUnityContainer container, IRegionManager regionManager ) : base(container, regionManager)
         {
-            this.MainWindowType = typeof(Views.PluginView);
+            this.MainWindowType = typeof(Views.Main);
         }
 
         /// <summary>
