95b68f8697c5de9adf03d5e0388852cfaedd9dc3
przemekwa <przemyslaw.walkowski@bzwbk.pl>
2016-08-04 10:42:50 +0200

    Kompilacja ale jeszcze nie działa

--- WPFPluginsApp/WPFPlugin1/WpfPlugin1.cs
+++ WPFPluginsApp/WPFPlugin1/WpfPlugin1.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Prism.Regions;
+using WPFPluginsInfrastructure;
 
 namespace WPFPlugin1
 {
-    public class WPFPlugin1
+    public class WpfPlugin1 : Modularity
     {
+        public WpfPlugin1(IUnityContainer container, IRegionManager regionManager ) : base(container, regionManager)
+        {
+            this.MainWindowType = typeof(Views.Inputs);
+        }
+
+        /// <summary>
+        /// Checks the module active.
+        /// </summary>
+        /// <returns>True if there are any rights to the module.</returns>
+        public override bool CheckModuleActive()
+        {
+            return true;
+        }
+
+        public override MenuItem GetMenuItem() => new MenuItem
+        {
+            Header = "Plugin1"
+        };
     }
 }
